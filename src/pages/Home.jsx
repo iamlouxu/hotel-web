12e10c2b9b9be408d157e0341bd4e26a2dcf74d0
Adolph-hong <zxcv010806056@gmail.com>
2025-09-16 16:41:40 +0800

    feat:rename picture and init homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 const Home = () => {
     return (
         <>
-            <div>
-                <h2>oqwejroiwej</h2>
-            </div>
+            {/* 背景圖 */}
+            <section className="bg-[url('/background/home-bg-pic1.png')] bg-cover bg-center h-screen flex pt-[109px] px-[128px]">
+                {/* 大盒子 */}
+                <div className="flex">
+                    {/* 上下盒子 */}
+                    <div className="flex gap-[144px]">
+                        <div className="flex"></div>
+                        <div className="flex">
+                            
+                        </div>
+                    </div>
+                    <div className="flex"></div>
+                </div>
+            </section>
         </>
     );
 }
