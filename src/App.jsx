750e4856ea115572b090f9ede53857a783a4665d
Adolph-hong <zxcv010806056@gmail.com>
2025-09-16 15:04:04 +0800

    feat:add router code

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home" 
+import Checkout from "./pages/Checkout" 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </>
   );
 }
